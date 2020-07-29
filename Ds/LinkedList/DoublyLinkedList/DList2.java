27d05fa04e666a30c4143cd295704d2684559df6
Abhinav <abhinav13872@gmail.com>
2020-07-29 15:35:17 +0530

    29 july

---
+++ Ds/LinkedList/DoublyLinkedList/DList2.java
@@ -0,0 +1,116 @@
+package DOUBLYLINKEDLIST;
+import java.util.Scanner;
+
+public class DList2
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+Node head=null;								//START NODE
+System.out.print("ENTER THE NO. OF ELEMENTS IN A DOUBLYLINKEDLIST: ");
+int n=sc.nextInt();
+
+if(n > 0) System.out.print("SCAN DOUBLYLINKEDLIST ELEMENTS: ");
+
+while(n-- > 0) head=insertAtEnd(head,sc.nextInt());
+Print(head);									//FORWARD-PRINTING	
+
+System.out.println("#ROTATE THE DOUBLYLINKEDLIST BY K ELEMENTS:-");
+System.out.print("SCAN VALUE OF K: ");
+head=rotateByK(head,sc.nextInt());
+Print(head);									//FORWARD-PRINTING	
+
+}
+
+
+private static Node rotateByK(Node head, int k)
+{
+if(head != null)
+{
+Node tail=head;										//LAST NODE
+int len=1;
+while(tail.next != null)
+{
+tail=tail.next;
+len++;
+}
+
+if(k < 1 || k >= len) return head;
+
+tail.next=head;
+head.prev=tail;
+Node ptr=tail;
+while(k-- > 1) ptr=ptr.prev;
+
+head=ptr;									//UPDATING HEAD
+ptr.prev.next=null;
+ptr.prev=null;
+return head;									//NEW HEAD
+}
+System.out.println("#NOTHING TO ROTATE -> EMPTY DOUBLYLINKEDLIST");
+return head;
+}
+
+
+private static Node insertAtEnd(Node head,int data)				//INSERTION AT END
+{
+if(head != null)
+{
+Node tail=head;								//LAST NODE
+while(tail.next != null) tail=tail.next;
+
+Node temp=new Node(data);
+tail.next=temp;								//UPDATING LASTNODE's NEXT LINK
+temp.prev=tail;								//UPDATING NODE's PREVIOUS LINK
+return head;
+}
+
+head=new Node(data);
+return head;
+}
+
+
+private static void Print(Node head)						//FORWARD-PRINT
+{
+System.out.print("DOUBLYLINKEDLIST: ");
+while(head != null)
+{
+System.out.print(head.data+" ");
+head=head.next;
+}
+System.out.println("\n");
+}
+
+
+private static void backwardPrint(Node head)					//BACKWARD-PRINT
+{
+System.out.print("BACKWARD_PRINTING CIRCULAR_LINKEDLIST: ");
+Node tail=head;								//LAST-NODE
+while(tail.next != null) tail=tail.next;
+
+Node ptr=tail;
+while(ptr != null)
+{
+System.out.print(ptr.data+" ");
+ptr=ptr.prev;
+}
+
+System.out.println("\n");
+}
+}
+
+
+class Node
+{
+int data;									//NODE's DATA
+Node prev;									//NODE's PREVIOUS LINK
+Node next;									//NODE's NEXT LINK
+
+public Node(int data)
+{
+this.data=data;
+prev=null;
+next=null;
+}
+}
+//ROTATE DOUBLYLINKEDLIST BY K ELEMENTS
