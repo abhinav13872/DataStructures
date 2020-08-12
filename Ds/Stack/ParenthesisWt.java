f7c06a80627a1025eceb075fd23f6f2100cdeff4
Abhinav <abhinav13872@gmail.com>
2020-08-12 10:38:37 +0530

    12 Aug

--- Ds/Stack/ParenthesisWt.java
+++ Ds/Stack/ParenthesisWt.java
@@ -1,35 +1,49 @@
 package STACK;
 import java.util.Scanner;
-import java.util.*;
+import java.util.Stack;
+import java.util.HashMap;
 
 
 public class ParenthesisWt
 {
-//HashMap to store the ending 
-//index of every opening bracket 
-static HashMap<Integer,Integer> endIndex=new HashMap<Integer,Integer>(); 
+/*HashMap to store the ending 
+index of every opening bracket*/
+static HashMap<Integer,Integer> map=new HashMap<Integer,Integer>(); 
+
+public static void main(String[] args) 
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("EXPRESSION: ");
+String str = sc.nextLine().trim();
+
+//Update the closing Index
+getClosingIndex(str);
+System.out.println("WEIGHT OF EXPRESSION: "+calcWeight(str,0,str.length()-1));
+}
+
 
 /*
 Function to calculate and store
 the closing index of each opening 
 bracket in the parenthesis
 */
-
-public static void getClosingIndex(String s) 
+public static void getClosingIndex(String str) 
 {
-int n = s.length(); 
+Stack<Integer> s = new Stack<Integer>(); 
 
-Stack<Integer> st = new Stack<Integer>(); 
-
-for (int i = 0; i < n; i++)
+for(int i=0;i<str.length();i++)
 {
-if (s.charAt(i) == ')')
-{ 
+char c=str.charAt(i);
+
+if(c == '(') s.push(i);
+
+if(c == ')')
+{
 /*If it's a closing bracket, 
 pop index of it's corresponding 
 opening bracket*/ 
 
-int startIndex = st.pop(); 
+int startIndex=s.pop(); 
 
 /*
 Insert the index of opening 
@@ -37,44 +51,24 @@ bracket and closing bracket
 as key-value pair in the 
 hashmap
 */ 
-endIndex.put(startIndex, i); 
-} 
-else
-{
-/*If it's an opening bracket, 
-push it's index into the stack*/
-st.push(i); 
+map.put(startIndex,i); 
 }
 }
 }
 
+
 /*Function to return the weight of 
 parenthesis*/
-public static int calcWeight(String s,int low,int high) 
+public static int calcWeight(String s,int low,int high)
 {
 //Base case 
-if(low + 1 == high) return 1; 
-else
-{
+if(low+1 == high) return 1; 
+
 /*Mid refers to ending index of 
 opening bracket at index low*/
-int mid = endIndex.get(low); 
-if (mid == high) return 2*calcWeight(s,low + 1,high-1);  
-else return calcWeight(s, low, mid)+calcWeight(s,mid+1,high); 
+int mid=map.get(low);
+
+if (mid == high) return (2*calcWeight(s,low + 1,high-1));
+return (calcWeight(s,low,mid) + calcWeight(s,mid+1,high));
 }
 }
-
-
-public static void main(String[] args) 
-{ 
-Scanner sc=new Scanner(System.in);
-System.out.print("EXPRESSION: ");
-String input = sc.nextLine().trim(); 
-int n = input.length(); 
-
-//Update the closing Index
-getClosingIndex(input); 
-System.out.println("WEIGHT OF EXPRESSION: "+calcWeight(input,0,n-1)); 
-} 
-} 
-
