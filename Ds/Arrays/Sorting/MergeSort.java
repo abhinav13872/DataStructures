1d2784c46911776c62ca5d35d8721c6936efdd55
Abhinav <abhinav13872@gmail.com>
2020-08-29 19:48:58 +0530

    29 Aug

--- Ds/Arrays/Sorting/MergeSort.java
+++ Ds/Arrays/Sorting/MergeSort.java
@@ -46,57 +46,38 @@ Merge(arr,L,Mid,R);
 
 
 
-private static void Merge(int arr[],int l,int m,int r)
+private static void Merge(int arr[],int l,int mid,int r)
 {
 // Find sizes of two subarrays to be merged 
-int n1 = m - l + 1; 
-int n2 = r - m; 
+int n1=mid-l+1; 
+int n2=r-mid; 
 
 /* Create temp arrays */
-int L[] = new int [n1]; 
-int R[] = new int [n2]; 
+int L[]=new int[n1];
+int R[]=new int[n2];
 
 /*Copy data to temp arrays*/
-for (int i=0; i<n1; ++i) L[i] = arr[l + i]; 
-for (int j=0; j<n2; ++j) R[j] = arr[m + 1+ j]; 
+for(int i=0; i<n1; i++) L[i] = arr[l+i]; 
+for(int j=0; j<n2; j++) R[j] = arr[mid+1+j]; 
 
-/* Merge the temp arrays */
 
+// MERGE:-
 // Initial indexes of first and second subarrays 
-int i = 0, j = 0; 
+int i=0,j=0; 
 
 // Initial index of merged subarry array 
-int k = l;
-while (i < n1 && j < n2) 
+int k=l;
+while(i < n1 && j < n2) 
 {
-if (L[i] <= R[j]) 
-{ 
-arr[k] = L[i]; 
-i++; 
-} 
-else
-{ 
-arr[k] = R[j]; 
-j++; 
-} 
-k++; 
-} 
+if (L[i] <= R[j]) arr[k++] = L[i++];
+else arr[k++] = R[j++];
+}
 
 /* Copy remaining elements of L[] if any */
-while (i < n1) 
-{ 
-arr[k] = L[i]; 
-i++; 
-k++; 
-} 
+while(i < n1) arr[k++] = L[i++]; 
 
 /* Copy remaining elements of R[] if any */
-while (j < n2) 
-{ 
-arr[k] = R[j]; 
-j++; 
-k++; 
-} 
+while(j < n2) arr[k++] = R[j++]; 
 }
 
 
