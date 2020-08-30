f91b714077e6999f13fc5f7423d8478f6a0804f7
Abhinav <abhinav13872@gmail.com>
2020-08-30 20:44:55 +0530

    30 Aug

--- Ds/Arrays/Sorting/QuickSort.java
+++ Ds/Arrays/Sorting/QuickSort.java
@@ -22,51 +22,50 @@ System.out.print("ARRAY: ");
 for(int i=0;i<n;i++) arr[i]=sc.nextInt();
 
 sort(arr,0,arr.length-1);
-Print(arr);
 }
 }
-
 
 
 
 static  int partition(int arr[], int low, int high) 
 { 
-int pivot = arr[high];  
+int pivot = arr[low];
 int start=low;
 int end=high;
 
 while(start < end)
 {
-
-while(arr[start] <= pivot) start++;
-while(arr[end] > pivot) end--;
+while(start <= high && arr[start] <= pivot) start++;
+while(end >= 0 && arr[end] > pivot) end--;
 
 if(start < end)
 {
-//swap
 int temp=arr[start];
 arr[start]=arr[end];
 arr[end]=temp;
 }
 }
-//swap
+
 int temp=arr[low];
 arr[low]=arr[end];
 arr[end]=temp;
+
+Print(arr);
 return end;
 }
 
 
 
-static void sort(int arr[], int low, int high)
-{
-if(low < high)
-{
-int pi = partition(arr, low, high);
 
-System.out.println("=================>pi: "+pi);
-sort(arr, low, pi-1);
-sort(arr, pi+1, high);
+static void sort(int arr[], int low, int high)
+{ 
+if (low < high)
+{ 
+int loc = partition(arr, low, high);
+System.out.println("=========>loc: "+loc);
+
+sort(arr, low, loc-1);
+sort(arr, loc+1, high);
 }
 }
 
