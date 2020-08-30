7d0cbf93e36e4a0a8709e5aeeeb2337084e021cc
Abhinav <abhinav13872@gmail.com>
2020-08-30 15:57:39 +0530

    30 Aug

--- Ds/Arrays/Sorting/QuickSort.java
+++ Ds/Arrays/Sorting/QuickSort.java
@@ -21,22 +21,61 @@ int arr[]=new int[n];
 System.out.print("ARRAY: ");
 for(int i=0;i<n;i++) arr[i]=sc.nextInt();
 
-arr=sort(arr,n);
+sort(arr,0,arr.length-1);
+Print(arr);
 }
 }
 
 
 
-private static int[] sort(int arr[],int n)					//QUICK_SORT-METHOD
+
+static  int partition(int arr[], int low, int high) 
+{ 
+int pivot = arr[high];  
+int start=low;
+int end=high;
+
+while(start < end)
 {
-return arr;
+
+while(arr[start] <= pivot) start++;
+while(arr[end] > pivot) end--;
+
+if(start < end)
+{
+//swap
+int temp=arr[start];
+arr[start]=arr[end];
+arr[end]=temp;
+}
+}
+//swap
+int temp=arr[low];
+arr[low]=arr[end];
+arr[end]=temp;
+return end;
 }
 
 
 
-private static void Print(int arr[],int n)					//PRINT-METHOD
+static void sort(int arr[], int low, int high)
+{
+if(low < high)
+{
+int pi = partition(arr, low, high);
+
+System.out.println("=================>pi: "+pi);
+sort(arr, low, pi-1);
+sort(arr, pi+1, high);
+}
+}
+
+
+
+private static void Print(int arr[])						//PRINT-METHOD
 {
 System.out.print("ARRAY: ");
+int n=arr.length;
 for(int i=0;i<n;i++) System.out.print(arr[i] + ((i != n-1) ? " " : "\n\n"));
 }
 }
