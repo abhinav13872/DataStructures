d54c9b716dfc9873dea676d7994f650a32162623
Abhinav <abhinav13872@gmail.com>
2020-09-02 11:54:07 +0530

    2 Sep

--- Ds/Arrays/Sorting/BubbleSort.java
+++ Ds/Arrays/Sorting/BubbleSort.java
@@ -30,14 +30,25 @@ Print(arr,n);
 
 private static int[] sort(int arr[],int n)					//SORT-METHOD
 {
-for(int i=0;i<n;i++)
-for(int j=0;j<n-i-1;j++)
-if(arr[j]>arr[j+1])
+for(int i=0;i<n-1;i++)
 {
+int flag=0;
+
+for(int j=0;j<n-1-i;j++)
+if(arr[j] > arr[j+1])
+{
+//SWAP:-
 int temp=arr[j];
 arr[j]=arr[j+1];
 arr[j+1]=temp;
+
+//Update flag
+flag=1;
 }
+
+if(flag != 1) break;
+}
+
 return arr;
 }
 
@@ -49,3 +60,30 @@ System.out.print("ARRAY: ");
 for(int i=0;i<n;i++) System.out.print(arr[i] + ((i != n-1) ? " " : "\n\n"));
 }
 }
+/*
+#BUBBLE_SORT:-
+
+=>In this technique we compare adjacent two elements and compare..
+  ->If elements are in correct order then don't Swap and move ahead.
+  ->If elements aren't in correct order then Swap them.
+
+=>OBSERVATION
+  After Pass-1 we get one largest element is bubbledup
+  After Pass-2 second largest element is bubbledup
+  After Pass-3 third largest element is bubbledup
+  [bubbledup means they're at their correct position now]
+  so if the no. of elements are 4 then no. of passes required
+  will be "3" to sort the array...
+  
+=>OPTIMISATION
+  Here we observed that sometimes the array gets sorted before the
+  required no. of passes then to avoid unneccessary computations
+  we've used a "flag" to break the loop of PASSES.
+  ->when no swaping is done in any pass then break the loop since the 
+    array is sorted.
+    
+=>COMPLEXITY
+  BEST CASE   : O(n)
+  AVERAGE CASE: O(n^2)
+  WORST CASE  : O(n^2)
+**/
