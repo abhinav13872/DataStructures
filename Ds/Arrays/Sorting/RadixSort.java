d214726f93d0d580b5988a137427f5d5210670b6
Abhinav <abhinav13872@gmail.com>
2020-09-08 18:17:44 +0530

    8 Sep

--- Ds/Arrays/Sorting/RadixSort.java
+++ Ds/Arrays/Sorting/RadixSort.java
@@ -50,18 +50,29 @@ while(count-- != 0)
 {
 int b[][]=new int[arr.length][10];
 
-for(int i=0;i<arr.length;i++)
+
+for(int i=0; i<arr.length; i++)
 {
-int j=arr[i]/x %10;
+//Calculating position
+int j= arr[i]/x % 10;
+
+/*Inserting elements acc. to the position "j"
+  (Ones,Tens,Hundreds,..) in a 2-D Array**/
 b[i][j]=arr[i];
 }
 
 Print(b);
 
+
+/*Updating Array
+=>Empty the buckets one by one and update
+  the Array "arr".
+**/
 int l=0;
-for(int j=0;j<10;j++) for(int i=0;i<arr.length;i++) if(b[i][j] != 0) arr[l++]=b[i][j];
+for(int j=0; j<10; j++) for(int i=0; i<arr.length; i++) if(b[i][j] != 0) arr[l++]=b[i][j];
 Print(arr);
-x*=10;
+
+x *= 10;
 }
 
 return arr;
@@ -73,7 +84,7 @@ private static void Print(int arr[])
 {
 System.out.print("ARRAY: ");
 int n=arr.length;
-for(int i=0;i<n;i++) System.out.print(arr[i] + ((i != n-1) ? " " : "\n\n"));
+for(int i=0; i<n; i++) System.out.print(arr[i] + ((i != n-1) ? " " : "\n\n"));
 }
 
 
@@ -81,9 +92,9 @@ for(int i=0;i<n;i++) System.out.print(arr[i] + ((i != n-1) ? " " : "\n\n"));
 private static void Print(int b[][])
 {
 System.out.println("2D-ARRAY:- ");
-for(int i=0;i<b.length;i++)
+for(int i=0; i<b.length; i++)
 {
-for(int j=0;j<10;j++) System.out.print(b[i][j]+"	");
+for(int j=0; j<10; j++) System.out.print(b[i][j]+"	");
 System.out.println();
 }
 System.out.println("\n");
@@ -91,7 +102,17 @@ System.out.println("\n");
 
 }
 /*
-TESTCASES:-
+#RADIX_SORT:-
+=>Radix Sort algorithm is a kind of count Sort in which we
+  count the no. of digits and sort..
 
-12 345 123 98 104 32 78 56 87 49 57 63
+
+#TESTCASE:-
+ 12 345 123 98 104 32 78 56 87 49 57 63
+
+
+#TIME-COMPLEXITY:-
+ ->Best Case   : O(n*k)
+ ->Average Case: O(n*k)
+ ->Worst Case  : O(n*k)
 */
